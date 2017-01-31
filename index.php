949c0e21430bf4414988ba7187654a127fa09762
Tapsoba Aubain <tapsoba.aubain@gmail.com>
2017-01-31 12:22:40 +0100

    Ma vision

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   </head>
   <body>
     <p>Bienvenue, ici nous affinons plus votre recherche</p>
-  <form action="//google.fr/search" method="post">
+  <form action="//google.fr/search" method="get">
     <p>
       <input type="text" name="q" placeholder="Je veux connaitre..." />
       <input type="submit" value="Valider" />
