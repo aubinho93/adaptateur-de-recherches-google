73522de45f3abb2df133c2c226096a763d343f52
Tapsoba Aubain <tapsoba.aubain@gmail.com>
2017-01-31 12:21:29 +0100

    Ma vision

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+      <title>Googletiseur d'informations</title>
+  </head>
+  <body>
+    <p>Bienvenue, ici nous affinons plus votre recherche</p>
+  <form action="//google.fr/search" method="post">
+    <p>
+      <input type="text" name="q" placeholder="Je veux connaitre..." />
+      <input type="submit" value="Valider" />
+    </p>
+  </form>
+  </body>
+</html>
